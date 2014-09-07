697488417390198918b714d7e09774df82cb38a2
TokaLazy <cl.steeve@hotmail.fr>
2014-09-07 13:21:12 +0200

    animation

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,4 @@
-$(function() {
+function menuClick() {
 	$('.menu .link').click(function(event) {
 		event.preventDefault();
 
@@ -10,14 +10,22 @@ $(function() {
 		$('.menu .link').removeClass('actif');
 		that.addClass('actif');
 
-		if(/suis/.test(that.text()))
+		if(/suis/.test(that.text())) {
 			bg = 'rgb(204,0,51)';
-		else if(/éali/.test(that.text()))
+			sectionTop = $('#qui-suis-je').offset().top;
+		}
+		else if(/éali/.test(that.text())) {
 			bg = 'rgb(0,204,153)';
-		else if(/vice/.test(that.text()))
+			sectionTop = $('#realisations').offset().top;
+		}
+		else if(/vice/.test(that.text())) {
 			bg = 'rgb(255,204,0)';
-		else if(/tact/.test(that.text()))
+			sectionTop = $('#services').offset().top;
+		}
+		else if(/tact/.test(that.text())) {
 			bg = 'rgb(0,102,153)';
+			sectionTop = $('#contact').offset().top;
+		}
 		else
 			bg = 'rgb(255,255,255)';
 
@@ -30,6 +38,32 @@ $(function() {
 				'left' : thisLeft,
 				'width' : thisWidth
 			});
-	});
 
+		$('html,body').animate({scrollTop : sectionTop - $('.nav').height() - parseInt($('.section').css('padding-top')) });
+	});
+}
+function apparence() {
+	$('.header, .section').css('height', windowHeight);
+
+	$(window).scroll(function() {
+		if($(window).scrollTop() > windowHeight) {
+			$('.nav').css({
+				'position' : 'fixed',
+				'top' : 0
+			});
+
+			$('.section').css('top', $('.nav').height());
+		}
+		else {
+			$('.nav').css('position','relative');
+			$('.section').css('top', 0);
+		}
+	});
+}
+
+windowHeight = $(window).height();
+
+$(function() {
+	menuClick();
+	apparence();
 });
